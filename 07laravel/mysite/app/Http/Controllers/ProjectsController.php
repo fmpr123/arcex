77267a4b87d69d893c0b78cd78f287a19bb851e5
fmpr123 <fabriciomicael@hotmail.com>
2019-11-14 17:44:03 +0000

    qwe

--- 07laravel/mysite/app/Http/Controllers/ProjectsController.php
+++ 07laravel/mysite/app/Http/Controllers/ProjectsController.php
@@ -7,19 +7,61 @@ use App\Project;
 
 class ProjectsController extends Controller
 {
-    public function index(){
-        $projects=Project::all();
-        return view('projects.index',['projects'=>$projects]);
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects.index', ['projects' => $projects]);
         //return $projects; retorna json com todos os dados
     }
-    public function create(){
+
+    public function create()
+    {
         return view('projects.create');
+
     }
-    public function store(){
-        $project=new Project();
+
+    public function store()
+    {
+        Project::create([
+            'title' => request('title'),
+            'description' => request('description')
+            ]);
+            return redirect('/projects');
+    }
+    
+    public function edit($id)
+    {
+        $project = Project::findOrFail($id);
+        return view('projects.edit', compact('project'));
+    }
+
+    public function update($id)
+    {
+        $project = Project::find($id);
         $project->title=request('title');
         $project->description=request('description');
         $project->save();
         return redirect('/projects');
+
+    }
+
+    public function destroy($id)
+    {
+        $project = Project::find($id);
+        $project->title=request('title');
+        $project->description=request('description');
+        $project->delete();
+        return redirect('/projects');
+    }
+
+    public function showAlt(Project $project)
+    {
+        return view('projects.show', compact('project')); 
+        #['project' => $project];
+    }
+
+    public function show($id){
+        $project = Project::findOrFail($id);
+        return view('projects.show',compact('project'));
     }
 }
